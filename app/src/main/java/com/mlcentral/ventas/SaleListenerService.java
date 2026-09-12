dda4695622bd29276e1481d5c07d3715490181ae
Jcardozo2013 <132786250+Jcardozo2013@users.noreply.github.com>
2026-09-12 08:45:11 -0300

    v1.21: conservar IDs públicos de canales

--- app/src/main/java/com/mlcentral/ventas/SaleListenerService.java
+++ app/src/main/java/com/mlcentral/ventas/SaleListenerService.java
@@ -5,4 +5,6 @@ package com.mlcentral.ventas;
  * Desde v1.21 usa el servicio Firebase robusto para celulares lentos.
  */
 public class SaleListenerService extends SafeFirebaseListenerService {
+    public static final String SALES_CHANNEL = "mlc_sales_urgent";
+    public static final String DELIVERY_CHANNEL = "mlc_delivery_status_v1";
 }
